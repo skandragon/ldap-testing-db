e7a11c6d1930ff659808b19d602e39743bb83de0
Michael Graff <explorer@flame.org>
2020-05-20 00:20:47 -0500

    need a better build system...

--- build-image.sh
+++ build-image.sh
@@ -1,1 +1,1 @@
-docker build -t skandragon/ldap-testing-db:latest . && docker images skandragon/ldap-testing-db:amd64-latest
+docker build -t skandragon/ldap-testing-db:arm64-latest . && docker images skandragon/ldap-testing-db:amd64-latest
